20d50d829ea260b06fe4287619737183b2492662
Mohamed Gaber Abdel-Ghani <141949552+Mohamed-Gaber53@users.noreply.github.com>
2025-10-05 03:26:22 -0700

    "Update App.jsx to include FavoritesList and RecommendationsList with Router"

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,26 +1,25 @@
-import { BrowserRouter as Router, Routes, Route, useParams } from "react-router-dom";
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
-import SearchBar from "./components/SearchBar";
-
-function RecipeDetailsWrapper() {
-  const { id } = useParams();
-  return <RecipeDetails recipeId={id} />;
-}
+import FavoritesList from "./components/FavoritesList";
+import RecommendationsList from "./components/RecommendationsList";
 
 function App() {
   return (
     <Router>
-      <SearchBar />
       <Routes>
         <Route
           path="/"
           element={
-            <>
+            <div style={{ padding: "20px" }}>
+              <h1>Recipe Sharing App</h1>
               <AddRecipeForm />
+              <FavoritesList />
+              <RecommendationsList />
               <RecipeList />
-            </>
+            </div>
           }
         />
         <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
@@ -28,5 +27,12 @@ function App() {
     </Router>
   );
 }
+
+// Wrapper to extract recipeId from URL and pass to RecipeDetails
+import { useParams } from "react-router-dom";
+const RecipeDetailsWrapper = () => {
+  const { id } = useParams();
+  return <RecipeDetails recipeId={id} />;
+};
 
 export default App;
