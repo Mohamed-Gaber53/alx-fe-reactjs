10340fe47e86cf526a5d8d4145d26a3ca7018c7a
Mohamed Gaber Abdel-Ghani <141949552+Mohamed-Gaber53@users.noreply.github.com>
2025-10-15 09:00:47 -0700

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import RecipeDetail from "./components/RecipeDetail";
+import AddRecipeForm from "./components/AddRecipeForm";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* الصفحة الرئيسية */}
         <Route path="/" element={<HomePage />} />
+
+        {/* تفاصيل وصفة */}
         <Route path="/recipe/:id" element={<RecipeDetail />} />
+
+        {/* إضافة وصفة جديدة */}
+        <Route path="/add" element={<AddRecipeForm />} />
       </Routes>
     </Router>
   );
