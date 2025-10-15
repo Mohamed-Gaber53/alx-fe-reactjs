c96b187e8e7d5550a4bf3b9d1f61a7c6f51bfe3a
Mohamed Gaber Abdel-Ghani <141949552+Mohamed-Gaber53@users.noreply.github.com>
2025-10-15 08:46:06 -0700

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,8 +1,10 @@
+import HomePage from "./components/HomePage";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-500">
-      Hello Tailwind CSS!
-    </h1>
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
